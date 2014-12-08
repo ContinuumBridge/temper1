955cd45dd3a8f1b5b5570aee4b1027854930ebc3
PeterClaydon <peter.claydon@continuumbridge.com>
2014-12-08 07:37:58 +0000

    Initial version

---
+++ adaptor_a.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# adaptor_a.py
+# Copyright (C) ContinuumBridge Limited, 2014 - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Peter Claydon
+#
+ModuleName               = "temperl"
+SENSOR_POLL_INTERVAL     = 30      # How often to request sensor values
+
+import sys
+import time
+import os
+from pprint import pprint
+import logging
+from cbcommslib import CbAdaptor
+from cbconfig import *
+from twisted.internet import threads
+from twisted.internet import reactor
+import subprocess
+
+class Adaptor(CbAdaptor):
+    def __init__(self, argv):
+        logging.basicConfig(filename=CB_LOGFILE,level=CB_LOGGING_LEVEL,format='%(asctime)s %(message)s')
+        self.status =           "ok"
+        self.state =            "stopped"
+        self.apps =             {"temperature": [],
+                                 "connected": []}
+        reactor.callLater(SENSOR_POLL_INTERVAL, self.pollSensor)
+        # super's __init__ must be called:
+        #super(Adaptor, self).__init__(argv)
+        CbAdaptor.__init__(self, argv)
+ 
+    def setState(self, action):
+        # error is only ever set from the running state, so set back to running if error is cleared
+        if action == "error":
+            self.state == "error"
+        elif action == "clear_error":
+            self.state = "running"
+        logging.debug("%s %s state = %s", ModuleName, self.id, self.state)
+        msg = {"id": self.id,
+               "status": "state",
+               "state": self.state}
+        self.sendManagerMessage(msg)
+
+    def sendcharacteristic(self, characteristic, data, timeStamp):
+        msg = {"id": self.id,
+               "content": "characteristic",
+               "characteristic": characteristic,
+               "data": data,
+               "timeStamp": timeStamp}
+        for a in self.apps[characteristic]:
+            self.sendMessage(msg, a)
+
+    def pollSensorThread(self):
+        # Called in thread
+        try:
+            t1 = subprocess.check_output(["temper-poll", "-q", "-c"])
+        except Exception as ex:
+            logging.warning('%s pollSensorTread. Failed to run temper-poll', ModuleName)
+            logging.warning("%s Exception: %s %s", ModuleName, type(ex), str(ex.args))
+        t = float(t1)
+        reactor.callFromThread(self.sendcharacteristic("temperature", t, time.time())
+
+    def pollSensor(self):
+        reactor.callInThread(self.pollSensorThread)
+        reactor.callLater(SENSOR_POLL_INTERVAL, self.pollSensor)
+
+    def onAppInit(self, message):
+        logging.debug("%s %s %s onAppInit, req = %s", ModuleName, self.id, self.friendly_name, message)
+        resp = {"name": self.name,
+                "id": self.id,
+                "status": "ok",
+                "service": [{"characteristic": "temperature", "interval": SENSOR_POLL_INTERVAL},
+                            {"characteristic": "connected", "interval": 0}],
+                "content": "service"}
+        self.sendMessage(resp, message["id"])
+        self.setState("running")
+
+    def onAppRequest(self, message):
+        logging.debug("%s %s %s onAppRequest, message = %s", ModuleName, self.id, self.friendly_name, message)
+        # Switch off anything that already exists for this app
+        for a in self.apps:
+            if message["id"] in self.apps[a]:
+                self.apps[a].remove(message["id"])
+        # Now update details based on the message
+        for f in message["service"]:
+            if message["id"] not in self.apps[f["characteristic"]]:
+                self.apps[f["characteristic"]].append(message["id"])
+        logging.debug("%s %s %s apps: %s", ModuleName, self.id, self.friendly_name, str(self.apps))
+
+    def onAppCommand(self, message):
+        logging.debug("%s %s %s onAppCommand, req = %s", ModuleName, self.id, self.friendly_name, message)
+        if "data" not in message:
+            logging.warning("%s %s %s app message without data: %s", ModuleName, self.id, self.friendly_name, message)
+        else:
+            logging.warning("%s %s %s This is a sensor. Message not understood: %s", ModuleName, self.id, self.friendly_name, message)
+
+    def onConfigureMessage(self, config):
+        """Config is based on what apps are to be connected.
+            May be called again if there is a new configuration, which
+            could be because a new app has been added.
+        """
+        logging.debug("%s onConfigureMessage, config: %s", ModuleName, config)
+        self.setState("starting")
+
+if __name__ == '__main__':
+    Adaptor(sys.argv)
